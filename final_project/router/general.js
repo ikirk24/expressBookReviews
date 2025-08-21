f61b4393169986ebc89135ae590a63b4311ea615
Ian Kirk <iankirk24@gmail.com>
2025-08-21 15:33:13 -0400

    On task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,26 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let booksArray = Object.entries(books);
+
+
+  
+
+  return res.send(booksArray);
 });
 
 // Get all books based on title
